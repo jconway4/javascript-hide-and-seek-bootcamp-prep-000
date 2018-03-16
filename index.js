57a97ec166c8884afe1d8755c087844c99efe436
jconway4 <jconwa4@illinois.edu>
2018-03-16 18:35:26 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+var getFirstSelector = function (selector) {
+  return document.querySelector(selector);
+};
+
+var nestedTarget = function () {
+  return document.querySelector('#nested .target');
+};
+
+var increaseRankBy = function(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+};
+
+var deepestChild = function() {
+  var find = function(array, criteriaFN) {
+    let current = array;
+    let next = [];
+    
+    while (current) {
+      if (criteriaFN(current)) {
+        return current;
+      }
+      
+      if (Array.isArray(current)) {
+        for (let i = 0; i < current.length; i++) {
+          next.push(current[i]);
+        }
+      }
+    current = next.shift();
+    }
+    return null;
+  };
+  
+  return find (
+    document.getElementById('grand-node').querySelectorAll('div'),
+    function(){n => n > 5})
+};
